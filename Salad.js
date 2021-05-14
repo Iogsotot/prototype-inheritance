54d0efc9902d3035502f438e22b34a17c600aa88
Iogsotot <ajustusa@gmail.com>
2021-05-14 20:24:25 +0300

    feat: add constants & foods templates

---
+++ Salad.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var SALAD = {
+  CAESAR: {
+    cost: 100,
+    kcal: 20,
+  },
+  RUSSIAN_SALAD: {
+    cost: 50,
+    kcal: 80,
+  },
+};
---
+++ myAwesomeSolution.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var order = (position) => {
+
+}
+
